9728659037603d0393ee00f6eec25c9f6dfeb320
bhgubhgu <bbhgu@naver.com>
2018-03-12 16:29:08 +0900

    Old Project ArcaneArena Script

--- bbhguScripts_New/AAObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class AAObject : MonoBehaviour //경기장 내의 게임오브젝트의 최상위 부모
-{
-   
-}
