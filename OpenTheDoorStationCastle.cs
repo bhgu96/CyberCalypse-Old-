e98c68f028540ce4bfefefd54ac9e75f127307ca
bhgubhgu <bbhgu@naver.com>
2018-03-12 16:37:30 +0900

    Old Project Old Script

---
+++ OpenTheDoorStationCastle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class  OpenTheDoorStationCastle : MonoBehaviour 
+{
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player" && Input.GetAxisRaw("Vertical") > 0.0f)
+		{
+			SceneManager.LoadScene("Lobby");
+		}
+	}
+}
---
+++ PlayerStopInStationStart.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStopInStationStart : MonoBehaviour 
+{
+	/* 필드 */
+	//자식인 HP Bar도 비활성화 되어있어야 한다.
+	//SetActive를 이용하여 자식인 HP는 처음엔 비활성화
+	public float titleAlphaColor;
+	Rigidbody2D rigid2d;
+	SpriteRenderer playerRender;
+	public GameObject  inactiveChildObject;
+
+	/* 메서드 */
+
+	//title의 alpha가 0이 되면 그때 player의 sprite 활성화, gravityscale = 2.5, HPBar 활성화
+	private void Start()
+	{
+		playerRender = GetComponent<SpriteRenderer>();
+		rigid2d = GetComponent<Rigidbody2D>();
+
+		rigid2d.isKinematic = true;
+		inactiveChildObject.gameObject.SetActive(false);
+	}
+	private void Update()
+	{
+		titleAlphaColor = TitleDownFade.titleAlpha;
+		if(TitleDownFade.titleAlpha > 0.0f)
+		{
+			playerRender.enabled = false;
+			rigid2d.gravityScale = 0f;	
+		}
+		else if(TitleDownFade.titleAlpha <= 0.0f)
+		{
+			playerRender.enabled = true;
+			inactiveChildObject.gameObject.SetActive(true);
+			rigid2d.isKinematic = false;
+			rigid2d.gravityScale = 2.5f;	
+		}
+	}
+	
+}
